4145a259bab60f22857da874c16ac662244977b9
Gusev-Art <artemii.gusev@mail.ru>
2021-04-25 12:10:24 -0400

    write rook

--- src/libchessviz/game.c
+++ src/libchessviz/game.c
@@ -72,13 +72,13 @@ bool _move(motion the_motion, char chess[8][8])
         result = moveN(the_motion, chess);
         break;
     case 'Q':
-        //  result = moveQ(the_motion, chess);
+        // result = moveQ(the_motion, chess);
         break;
     case 'K':
         //  result = moveK(the_motion, chess);
         break;
     case 'R':
-        //  result = moveR(the_motion, chess);
+        result = moveR(the_motion, chess);
         break;
     }
     return result;
